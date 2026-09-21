8ca9394fef3a27037960412a2bac6de0cce1ce0a
Caremi systems <gomezlopezalfredo.edu@gmail.com>
2026-09-21 00:44:45 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // OPTICST — configuración del backend
 // Después de desplegar Apps Script como Web App, coloca aquí la URL /exec.
-window.OPTICST_API_URL = "PEGAR_AQUI_URL_DEL_WEB_APP_EXEC";
+window.OPTICST_API_URL = "https://script.google.com/macros/s/AKfycbyLorfJ8lY0JtvKd6EMwEQj401oSb5BhWDPcTzhbGcS3eeGxZuhv97BZ5RyR4EI_JoZyg/exec";
